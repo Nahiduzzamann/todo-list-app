788e01fe26821d7810326f0b0849eba8aed9358c
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-03-16 12:57:20 +0600

    ok

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -4,13 +4,18 @@ import TaskItem from './TaskItem';
 const TaskList = () => {
   const tasks = useSelector(state => state.todo.tasks);
 
+  const totalTasks = tasks.length;
+  const completedTasks = tasks.filter(task => task.completed).length;
+
   return (
     <div className='px-2'>
-      <h1 className='text-blue-500 text-xl md:text-3xl underline'>Task List:</h1>
+      <h1 className='text-blue-500 text-xl md:text-3xl underline'>Task List</h1>
       <div className='m-2 md:m-4'>
-      {tasks.map(task => (
-        <TaskItem key={task.id} task={task} />
-      ))}
+        <p>Total Tasks: {totalTasks}</p>
+        <p>Completed Tasks: {completedTasks}</p>
+        {tasks.map(task => (
+          <TaskItem key={task.id} task={task} />
+        ))}
       </div>
     </div>
   );
