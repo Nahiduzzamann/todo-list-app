68490246bf7e3b50546d069928f0a4f3509bebb9
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-03-16 19:38:13 +0600

    done

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import TaskItem from "./TaskItem";
 import { Select, Pagination } from "antd";
+import svg from "../assets/task.svg";
 const { Option } = Select;
 
 const TaskList = () => {
@@ -34,35 +35,42 @@ const TaskList = () => {
     (currentPage - 1) * pageSize,
     currentPage * pageSize
   );
-
+  // console.log(tasks);
   return (
     <div className="px-2">
-      <h1 className="text-blue-500 text-xl md:text-3xl underline">Task List</h1>
-      <div className="m-2 md:m-4">
-        <div className="flex justify-between items-center">
-          <div>
-            <PriorityFilter
-              selectedPriority={selectedPriority}
-              onChange={handlePriorityChange}
+      <h1 className="text-gray-500 text-xl md:text-3xl">Task List</h1>
+      {tasks.length <= 0 ? (
+        <div className="flex flex-col justify-center items-center my-4">
+          <img className="h-40 w-40" src={svg} alt="empty" />
+          <p className="text-gray-400 text-lg">No Task</p>
+        </div>
+      ) : (
+        <div className="m-2 md:m-4">
+          <div className="flex justify-between items-center">
+            <div>
+              <PriorityFilter
+                selectedPriority={selectedPriority}
+                onChange={handlePriorityChange}
+              />
+            </div>
+            <div>
+              <p>Total Tasks: {totalTasks}</p>
+              <p>Completed Tasks: {completedTasks}</p>
+            </div>
+          </div>
+          {paginatedTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
+          <div className="flex justify-center mt-2">
+            <Pagination
+              current={currentPage}
+              pageSize={pageSize}
+              total={getFilteredTasks().length}
+              onChange={handleChangePage}
             />
           </div>
-          <div>
-            <p>Total Tasks: {totalTasks}</p>
-            <p>Completed Tasks: {completedTasks}</p>
-          </div>
         </div>
-        {paginatedTasks.map((task) => (
-          <TaskItem key={task.id} task={task} />
-        ))}
-        <div className="flex justify-center mt-2">
-          <Pagination
-            current={currentPage}
-            pageSize={pageSize}
-            total={getFilteredTasks().length}
-            onChange={handleChangePage}
-          />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
