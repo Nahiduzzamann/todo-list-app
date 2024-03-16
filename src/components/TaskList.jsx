9fa2f5c62589431ffdd0758232c262deeeb88564
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-03-16 12:26:21 +0600

    update task list

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -5,10 +5,13 @@ const TaskList = () => {
   const tasks = useSelector(state => state.todo.tasks);
 
   return (
-    <div>
+    <div className='px-2'>
+      <h1 className='text-blue-500 text-xl md:text-3xl underline'>Task List:</h1>
+      <div className='m-2 md:m-4'>
       {tasks.map(task => (
         <TaskItem key={task.id} task={task} />
       ))}
+      </div>
     </div>
   );
 };
