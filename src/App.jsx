bec30741c90108bfbb7a5bb9598cb2d1a7c98ee9
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-03-16 10:10:30 +0600

    basic set up and installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import "./App.css";
+// App.js
+import  { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Layout, Typography } from 'antd';
+import { addTask } from './store/todoSlice';
+import './App.css';
 
-function App() {
-  return <div>Home</div>;
-}
+const { Header, Content } = Layout;
+const { Title } = Typography;
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (savedTasks) {
+      savedTasks.forEach(task => dispatch(addTask(task)));
+    }
+  }, [dispatch]);
+
+  return (
+    <Layout>
+      <Header>
+        <Title style={{ color: 'white' }}>Todo List App</Title>
+      </Header>
+      <Content className="content">
+        {/* <TaskForm /> */}
+        Task Form
+        {/* <FilterDropdown /> */}
+        FilterDropdown
+        {/* <TaskList /> */}
+        TaskList
+      </Content>
+    </Layout>
+  );
+};
 
 export default App;
