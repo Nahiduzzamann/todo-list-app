2f78790fb106e5de55dd85a0b0432e2e5c57db20
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2024-03-16 14:19:16 +0600

    ok

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,22 +1,30 @@
-import { useSelector } from 'react-redux';
-import TaskItem from './TaskItem';
+import { useSelector } from "react-redux";
+import TaskItem from "./TaskItem";
 
 const TaskList = () => {
-  const tasks = useSelector(state => state.todo.tasks);
+  const tasks = useSelector((state) => state.todo.tasks);
 
   const totalTasks = tasks.length;
-  const completedTasks = tasks.filter(task => task.completed).length;
+  const completedTasks = tasks.filter((task) => task.completed).length;
 
-  const incompleteTasks = tasks.filter(task => !task.completed);
-  const sortedTasks = [...incompleteTasks, ...tasks.filter(task => task.completed)];
+  const incompleteTasks = tasks.filter((task) => !task.completed);
+  const sortedTasks = [
+    ...incompleteTasks,
+    ...tasks.filter((task) => task.completed),
+  ];
 
   return (
-    <div className='px-2'>
-      <h1 className='text-blue-500 text-xl md:text-3xl underline'>Task List</h1>
-      <div className='m-2 md:m-4'>
-        <p>Total Tasks: {totalTasks}</p>
-        <p>Completed Tasks: {completedTasks}</p>
-        {sortedTasks.map(task => (
+    <div className="px-2">
+      <h1 className="text-blue-500 text-xl md:text-3xl underline">Task List</h1>
+      <div className="m-2 md:m-4">
+        <div className="flex justify-between items-center">
+          <div>Basic Filtering system</div>
+          <div>
+            <p>Total Tasks: {totalTasks}</p>
+            <p>Completed Tasks: {completedTasks}</p>
+          </div>
+        </div>
+        {sortedTasks.map((task) => (
           <TaskItem key={task.id} task={task} />
         ))}
       </div>
